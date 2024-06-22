587f209adcc62c2b95f8416c0ea5cfb905ec2883
Daniel Canon <danielycm02@gmail.com>
2024-06-21 20:52:08 -0500

    clase_3

--- MODULO_2/JS_C3/workclass/segunda_parte/main.js
+++ MODULO_2/JS_C3/workclass/segunda_parte/main.js
@@ -17,14 +17,21 @@
 // Calcular el precio de entrada a un evento, el programa debe preguntar el genero (m/f/o) y tambien
 // preguntar la edad
 
-const genero = prompt("Digita tu genero (m/f/o)");
-const age = Number(prompt("Digita tu edad"));
-//  (true || false) = (true)
-if(genero === "f" || genero === "m" || age >= 64 ){
-    document.write("Tu entrada tiene un precio de $10.000")
-} else {
-    document.write("Tu entrada tiene un precio de $20.000")
+// const genero = prompt("Digita tu genero (m/f/o)");
+// const age = Number(prompt("Digita tu edad"));
+// //  (true || false) = (true)
+// if(genero === "f" || genero === "m" || age >= 64 ){
+//     document.write("Tu entrada tiene un precio de $10.000")
+// } else {
+//     document.write("Tu entrada tiene un precio de $20.000")
+// }
+
+const suscripcion = false;
+if(suscripcion){
+    document.write("Felicidades")   
 }
+
+
 
 
 
