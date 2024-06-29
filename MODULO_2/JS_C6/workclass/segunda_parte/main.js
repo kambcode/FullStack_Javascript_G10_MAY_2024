580bc8470bd7b9173ac1a1ee977c2c7afda70968
Daniel Canon <danielycm02@gmail.com>
2024-06-28 20:58:18 -0500

    clase_6

--- MODULO_2/JS_C6/workclass/segunda_parte/main.js
+++ MODULO_2/JS_C6/workclass/segunda_parte/main.js
@@ -3,3 +3,17 @@
 
 // frontend: Todo es muy visual (imagen, listas, titulos, colores)
 // backend: No es para nada visual (codigo), es similar a lo que pasa en la nube
+
+// RETO: Descargar node.js
+
+// 1. click derecho al archivo que deseamos ejecutar
+// 2. dar click en "open in integrated terminal"
+// 3. node main.js => Para ejecutar en la consola con el nombre del archivo
+
+
+
+console.log("Hola mundo")
+
+// document.write("Hola como estas") => No se puede utilizar en la consola o terminal
+// prompt("Digita un numero") //=> No se puede utilizar en la consola o terminal
+// alert("Digita un numero") //=> No se puede utilizar en la consola o terminal
