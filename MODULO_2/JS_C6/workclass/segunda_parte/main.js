65b4dbc67c12a30f04421137cf039cdfa27da782
Daniel Canon <danielycm02@gmail.com>
2024-06-28 20:25:03 -0500

    clase_6

---
+++ MODULO_2/JS_C6/workclass/segunda_parte/main.js
@@ -0,0 +1,5 @@
+// backend: es lo de atras, cuantos usiario hay registrados
+// contraseñas, productos, envios de correos por las compras
+
+// frontend: Todo es muy visual (imagen, listas, titulos, colores)
+// backend: No es para nada visual (codigo), es similar a lo que pasa en la nube
