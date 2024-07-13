e4fa70f0b84017ac92f240145d78682b7858b38d
Daniel Canon <danielycm02@gmail.com>
2024-07-12 21:05:45 -0500

    clase_11

--- MODULO_2/JS_C7/solucion/main.js
+++ MODULO_2/JS_C7/solucion/main.js
@@ -4,7 +4,7 @@ for(let i = 1; i <= 5; i++){
     let producto = prompt("Digita un producto")
     if(lista.includes(producto)){
         alert("Error: Este elementos ya esta en la lista de compras")
-        i = i - 1
+        i = i - 1 
     } else {
         lista.push(producto);
     }
